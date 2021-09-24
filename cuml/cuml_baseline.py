4e70e8c676619208037fb3c438041da8af09311c
unknown <mkshah5@ncsu.edu>
2021-09-24 14:42:56 -0400

    timing cuml

--- cuml/cuml_baseline.py
+++ cuml/cuml_baseline.py
@@ -28,7 +28,7 @@ for i in range(len(DATASET_NAME)):
 
     for j in range(len(DEPTHS)):
         results_file.write("td: " +str(DEPTHS[i])+" ne: "+str(NUM_ESTIMATORS[i])+ "\n")
-        model = load_objects("MODEL"+DATASET_NAME+"_td"+str(DEPTHS[j])+"_ne"+str(NUM_ESTIMATORS[j]))[0]
+        model = load_objects("MODEL"+DATASET_NAME[i]+"_td"+str(DEPTHS[j])+"_ne"+str(NUM_ESTIMATORS[j]))[0]
 
         X_gpu = cuda.to_device(np.ascontiguousarray(X_test.astype(np.float32)))
         
