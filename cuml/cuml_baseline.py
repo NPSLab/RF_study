32166a16f36a821405d2aca36250b78e58c97c59
unknown <mkshah5@ncsu.edu>
2021-09-25 15:45:24 -0400

    covtype setup

--- cuml/cuml_baseline.py
+++ cuml/cuml_baseline.py
@@ -1,4 +1,5 @@
 # from data.synthetic.rf_layout import MAX_ESTIMATORS
+from data.test_tree_depth.tree_depth_testing import DATASET_PATH
 from pickle import load
 import sklearn, sklearn.datasets, numpy as np
 from numba import cuda
@@ -9,27 +10,38 @@ import time
 from utilities import save_objects
 from utilities import load_objects
 from utilities import loadcsr_from_txt
+from libsvm.svmutil import *
+from sklearn.ensemble import RandomForestClassifier
 
 
 
 DIRNAME = os.path.dirname(__file__)
 
 # MODEL_PATH = "data\test_tree_depth\Covtype_trained"
+DATASET_PATH = "covtype.libsvm.binary"
 DATASET_NAME = ["covtype"]
 DEPTHS = [25, 45, 45]
 NUM_ESTIMATORS = [50, 50, 100]
 
-for i in range(len(DATASET_NAME)):
-    testset_name = "TESTSET"+DATASET_NAME[i]
+y_all, x_all = svm_read_problem(DATASET_PATH, return_scipy = True)
+x_all = np.asarray(x_all)
+y_all = y_all.to_numpy().ravel()
 
-    X_test, y_test = load_objects(testset_name)
+X, X_test, y, y_test = train_test_split(x_all, y_all, test_size=0.2)
+
+for i in range(len(DATASET_NAME)):
+    # testset_name = "TESTSET"+DATASET_NAME[i]
+
+    # X_test, y_test = load_objects(testset_name)
 
     results_file = open(DATASET_NAME[i]+"_cuml_results.txt", 'w')
 
     for j in range(len(DEPTHS)):
+        print("Running "+"td: " +str(DEPTHS[j])+" ne: "+str(NUM_ESTIMATORS[j]))
         results_file.write("td: " +str(DEPTHS[j])+" ne: "+str(NUM_ESTIMATORS[j])+ "\n")
-        model = load_objects("MODEL"+DATASET_NAME[i]+"_td"+str(DEPTHS[j])+"_ne"+str(NUM_ESTIMATORS[j]))[0]
-
+        # model = load_objects("MODEL"+DATASET_NAME[i]+"_td"+str(DEPTHS[j])+"_ne"+str(NUM_ESTIMATORS[j]))[0]
+        model = RandomForestClassifier(n_estimators= NUM_ESTIMATORS[j], max_depth = DEPTHS[j])
+        model.fit(X, y)
         X_gpu = cuda.to_device(np.ascontiguousarray(X_test.astype(np.float32)))
         
         fm = ForestInference.load_from_sklearn(model, output_class=True)
