2c4065333d5c0d8159b5500fce7ef0a49778f5b7
unknown <mkshah5@ncsu.edu>
2021-10-05 12:48:50 -0400

    updated shell

--- data/synthetic/one.sh
+++ data/synthetic/one.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 cp ./test_input.txt ./tree_input.txt
-depths = ( 20 40 45 45 45 60 )
-estimators = ( 50 50 10 50 100 50 )
+# depths = ( 20 40 45 45 45 60 )
+# estimators = ( 50 50 10 50 100 50 )
 for ((i=0;i<6;i++))
 do
-  tree_depth = ${depths[i]}
-  ne = ${estimators[i]}
+  tree_depth = 20
+  ne = 50
   echo
   echo "TTT$tree_depth: test tree depth $tree_depth"
   cp ./input_files/clustered_metadata_SUSY_td${tree_depth}_ne${ne}_csr.txt ./treefile_csr.txt
