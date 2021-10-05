9f93fd2b73990c155c83c8b032dc975310d2a817
unknown <mkshah5@ncsu.edu>
2021-10-05 13:01:32 -0400

    updated shell

--- data/synthetic/one.sh
+++ data/synthetic/one.sh
@@ -8,14 +8,14 @@ cp ./test_input.txt ./tree_input.txt
 for tree_depth in 20 21
 do
   # tree_depth = 20
-  ne = 50
+  # ne = 50
   echo
   echo "TTT$tree_depth: test tree depth $tree_depth"
-  cp ./input_files/clustered_metadata_SUSY_td${tree_depth}_ne${ne}_csr.txt ./treefile_csr.txt
+  cp ./input_files/clustered_metadata_SUSY_td${tree_depth}_ne50_csr.txt ./treefile_csr.txt
   ./gpu/rf_gpu_csr ${tree_depth}
 
   echo
   echo "TTT$tree_depth: test subtree depth 5"
-  cp ./input_files/clustered_metadata_SUSY_td${tree_depth}_ne${ne}_sd4_hier.txt ./treefile_hier.txt
+  cp ./input_files/clustered_metadata_SUSY_td${tree_depth}_ne50_sd4_hier.txt ./treefile_hier.txt
   ./gpu/rf_gpu_iter ${tree_depth}
 done
