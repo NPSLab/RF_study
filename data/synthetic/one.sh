4c2430430cb1b39dd284055a1b1792718e02270f
Mengxuan Ma <mmrnc@mail.missouri.edu>
2022-02-10 18:52:37 -0600

    Transferring old nodes array into two different layouts, cut the memory foot print by half for nodes array.

--- data/synthetic/one.sh
+++ data/synthetic/one.sh
@@ -2,10 +2,11 @@
 
 cp ./test_input.txt ./tree_input.txt
 
-for sd in 15 
+for sd in 10 
 do
   echo "TTT$sd: test tree depth $sd"
   cp ./td${sd}_csr.txt ./treefile_csr.txt
+  ./rf_gpu_csr
   echo
   #echo "TTT$sd: test subtree depth 3"
   #cp ./td${sd}sd2_hier.txt ./treefile_hier.txt
@@ -15,7 +16,11 @@ do
   #cp ./td${sd}sd3_hier.txt ./treefile_hier.txt
   #./rf_gpu
   #echo
-  #echo "TTT$sd: test subtree depth 5"
-  #cp ./td${sd}sd4_hier.txt ./treefile_hier.txt
-  #./rf_gpu
+  echo "TTT$sd: test subtree depth 5 with default layout"
+  cp ./td${sd}sd4_hier.txt ./treefile_hier.txt
+  ./rf_gpu_iter
+  echo "TTT$sd: test subtree depth 5 with new layout 1"
+  ./rf_gpu_iter_new_layout
+  echo "TTT$sd: test subtree depth 5 with new layout 2"
+  ./rf_gpu_iter_new_layout2
 done
